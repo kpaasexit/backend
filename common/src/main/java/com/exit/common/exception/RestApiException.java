02d9a62ff798b51408d5c17246db5387cb0815b4
taeyeongkims <xodud8530@gmail.com>
2025-09-01 15:28:04 +0900

    jwt 관련 유틸, 성공, 실패 공통 응답 파일

---
+++ common/src/main/java/com/exit/common/exception/RestApiException.java
@@ -0,0 +1,27 @@
+package com.exit.common.exception;
+
+import com.exit.common.response.error.ErrorCode;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class RestApiException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+    private final List<String> errors;
+
+    public RestApiException(ErrorCode errorCode) {
+        this(errorCode, null, null);
+    }
+
+    public RestApiException(ErrorCode errorCode, String detailMessage) {
+        this(errorCode, detailMessage, null);
+    }
+
+    public RestApiException(ErrorCode errorCode, String detailMessage, List<String> errors) {
+        super(detailMessage);
+        this.errorCode = errorCode;
+        this.errors = errors;
+    }
+}
---
+++ common/src/main/java/com/exit/common/response/error/AuthErrorCode.java
@@ -0,0 +1,18 @@
+package com.exit.common.response.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthErrorCode implements ErrorCode {
+    NULL_USER("AUTH_ERR_001", HttpStatus.BAD_REQUEST, "사용자가 존재하지 않습니다."),
+    INVALID_TOKEN("AUTH_ERR_002", HttpStatus.BAD_REQUEST, "올바르지 않은 토큰입니다."),
+    EXPIRED_TOKEN("AUTH_ERR_003", HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    INVALID_REDIRECT_URI("AUTH_ERR_004", HttpStatus.BAD_REQUEST, "유효하지 않은 리다이렉트 경로입니다.");
+
+    private final String developCode;
+    private final HttpStatus httpStatus;
+    private final String errorDescription;
+}
