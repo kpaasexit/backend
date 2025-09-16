6c242327f84bea66933b625f3a327913390a1136
taeyeongkims <xodud8530@gmail.com>
2025-09-16 15:22:36 +0900

    feat/ 파일 업로드 유틸리티 구현

---
+++ common/src/main/java/com/exit/common/file/util/FileUploadException.java
@@ -0,0 +1,30 @@
+package com.exit.common.file.util;
+
+public class FileUploadException extends RuntimeException {
+
+    public FileUploadException(String message) {
+        super(message);
+    }
+
+    public FileUploadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public static class InvalidFileTypeException extends FileUploadException {
+        public InvalidFileTypeException(String message) {
+            super(message);
+        }
+    }
+
+    public static class FileSizeExceededException extends FileUploadException {
+        public FileSizeExceededException(String message) {
+            super(message);
+        }
+    }
+
+    public static class FileUploadFailedException extends FileUploadException {
+        public FileUploadFailedException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
+}
