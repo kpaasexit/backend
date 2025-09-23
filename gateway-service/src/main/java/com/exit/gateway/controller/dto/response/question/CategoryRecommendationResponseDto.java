f57a130c2a830e3425e6ddc1bb676add8786ea85
taeyeongkims <xodud8530@gmail.com>
2025-09-23 20:03:51 +0900

    feat: 유사 질문, 카테고리 추천 제외 구현

---
+++ gateway-service/src/main/java/com/exit/gateway/controller/dto/response/question/CategoryRecommendationResponseDto.java
@@ -0,0 +1,17 @@
+package com.exit.gateway.controller.dto.response.question;
+
+import com.exit.common.grpc.CategoryRecommendationResponse;
+import lombok.Builder;
+
+@Builder
+public record CategoryRecommendationResponseDto(
+        Long questionCategoryId,
+        String questionCategoryName
+) {
+    public static CategoryRecommendationResponseDto from(CategoryRecommendationResponse categoryRecommendationResponse) {
+        return CategoryRecommendationResponseDto.builder()
+                .questionCategoryId(categoryRecommendationResponse.getCategoryId())
+                .questionCategoryName(categoryRecommendationResponse.getCategoryName())
+                .build();
+    }
+}
