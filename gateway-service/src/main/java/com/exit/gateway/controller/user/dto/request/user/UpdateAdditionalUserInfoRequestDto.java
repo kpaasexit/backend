9c48cc5d72813fbb87a8969edc19ff4decb71001
taeyeongkims <xodud8530@gmail.com>
2025-09-25 00:33:10 +0900

    feat: 사용자 추가 정보 api dto

---
+++ gateway-service/src/main/java/com/exit/gateway/controller/user/dto/request/user/UpdateAdditionalUserInfoRequestDto.java
@@ -0,0 +1,9 @@
+package com.exit.gateway.controller.user.dto.request.user;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public record UpdateAdditionalUserInfoRequestDto(
+        String nickname,
+        MultipartFile image
+) {
+}
