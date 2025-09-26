74febc85fd3b7de9d6d3c169a6be0b0b86b4d475
taeyeongkims <xodud8530@gmail.com>
2025-09-26 16:08:52 +0900

    feat: 매거진 관련 레포지토리들 도메인, 타입 위치 오류 변경

--- magazine-service/src/main/java/com/exit/magazine/domain/Repository/MagazineCategoryRepository.java
+++ magazine-service/src/main/java/com/exit/magazine/domain/Repository/MagazineCategoryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MagazineCategoryRepository extends JpaRepository<Long, MagazineCategories> {
+public interface MagazineCategoryRepository extends JpaRepository<MagazineCategories, Long> {
 }
