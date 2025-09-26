273580058fb2542a95e3fae8a353a5cb6a2150de
taeyeongkims <xodud8530@gmail.com>
2025-09-26 17:20:00 +0900

    feat: 매거진 API 응답 DTO 생성

---
+++ gateway-service/src/main/java/com/exit/gateway/controller/dto/response/MagazineItemListDto.java
@@ -0,0 +1,17 @@
+package com.exit.gateway.controller.dto.response;
+
+import com.exit.common.grpc.GetMagazinesByCategoryResponse;
+
+import java.util.List;
+
+public record MagazineItemListDto(
+        List<MagazineItemDto> magazineItems
+) {
+    public static MagazineItemListDto from(GetMagazinesByCategoryResponse response) {
+        List<MagazineItemDto> list = response.getMagazineItemList()
+                .stream()
+                .map(MagazineItemDto::from)
+                .toList();
+        return new MagazineItemListDto(list);
+    }
+}
