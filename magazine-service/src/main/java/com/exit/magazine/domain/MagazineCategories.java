29eae13591de8d2ba74b533f707a7c02c49f00dc
taeyeongkims <xodud8530@gmail.com>
2025-09-26 16:11:25 +0900

    feat: 매거진 관련 도메인에 @Getter 추가

--- magazine-service/src/main/java/com/exit/magazine/domain/MagazineCategories.java
+++ magazine-service/src/main/java/com/exit/magazine/domain/MagazineCategories.java
@@ -3,10 +3,12 @@ package com.exit.magazine.domain;
 import com.exit.common.domain.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "MagazineCategories")
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AttributeOverride(name = "createdAt", column = @Column(name = "magzine_category_created_at"))
 @AttributeOverride(name = "updatedAt", column = @Column(name = "magzine_category_updated_at"))
