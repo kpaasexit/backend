6e945ae9c798a330c43c4ccdab98e984616553fd
taeyeongkims <xodud8530@gmail.com>
2025-09-26 16:10:04 +0900

    feat: 매거진 성공 코드 추가

---
+++ common/src/main/java/com/exit/common/response/success/MagazineSuccessCode.java
@@ -0,0 +1,17 @@
+package com.exit.common.response.success;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum MagazineSuccessCode implements SuccessCode {
+    GET_MAGAZINE_LIST_SUCCESS("MAGAZINE_OK_001", HttpStatus.CREATED, "매거진 리스트 조회 성공"),
+    GET_MAGAZINE_SUCCESS("MAGAZINE_OK_002", HttpStatus.OK, "매거진 상세 조회 성공")
+    ;
+
+    private final String code;
+    private final HttpStatus httpStatus;
+    private final String message;
+}
