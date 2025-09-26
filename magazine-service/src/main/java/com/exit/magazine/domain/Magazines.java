b697d63ee980b3004966ac97f392325fa6763aa2
taeyeongkims <xodud8530@gmail.com>
2025-09-26 14:05:33 +0900

    feat: 매거진 서비스 도메인 생성
    - 매거진 도메인
    - 매거진 카테고리 도메인

---
+++ magazine-service/src/main/java/com/exit/magazine/domain/Magazines.java
@@ -0,0 +1,47 @@
+package com.exit.magazine.domain;
+
+import com.exit.common.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Magazines")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "createdAt", column = @Column(name = "magazine_created_at"))
+@AttributeOverride(name = "updatedAt", column = @Column(name = "magazine_updated_at"))
+public class Magazines extends BaseEntity {
+    @Id
+    @Column(name = "magazine_id")
+    private Long magazineId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "magazine_category_id")
+    private MagazineCategories magazineCategory;
+
+    @Column(name = "magazine_author_id")
+    private Long magazineAuthorId;
+
+    @Column(name = "magazine_title", length = 200)
+    private String magazineTitle;
+
+    @Column(name = "magazine_subtitle", length = 200)
+    private String magazineSubtitle;
+
+    @Column(name = "magazine_content", columnDefinition = "TEXT")
+    private String magazineContent;
+
+    @Column(name = "magazine_thumbnail_url", length = 500)
+    private String magazineThumbnailUrl;
+
+    @Builder
+    public Magazines(MagazineCategories category, Long authorId, String magazineTitle, String magazineSubtitle, String magazineContent, String magazineThumbnailUrl) {
+        this.magazineCategory = category;
+        this.magazineAuthorId = authorId;
+        this.magazineTitle = magazineTitle;
+        this.magazineSubtitle = magazineSubtitle;
+        this.magazineContent = magazineContent;
+        this.magazineThumbnailUrl = magazineThumbnailUrl;
+    }
+}
