2e829456d636942294d81d7478e95e91b3a15b58
taeyeongkims <xodud8530@gmail.com>
2025-09-27 15:58:08 +0900

    feat: 추가 질문 관련 도메인 헬퍼 메서드 및 생성자 추가

--- question-service/src/main/java/com/exit/question/domain/question/FollowUpImage.java
+++ question-service/src/main/java/com/exit/question/domain/question/FollowUpImage.java
@@ -3,8 +3,11 @@ package com.exit.question.domain.question;
 import com.exit.common.domain.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Table(name = "follow_up_images")
@@ -16,6 +19,7 @@ public class FollowUpImage extends BaseEntity {
 
     @Id
     @Column(name = "follow_up_image_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long followUpImageId;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -24,4 +28,16 @@ public class FollowUpImage extends BaseEntity {
 
     @Column(name = "follow_up_image_url", length = 512)
     private String followUpImageUrl;
+
+    @Builder
+    public FollowUpImage(FollowUpMessage followUpMessage, String followUpImageUrl) {
+        this.followUpMessage = followUpMessage;
+        this.followUpImageUrl = followUpImageUrl;
+    }
+
+    public static List<FollowUpImage> generateFollowUpImages(FollowUpMessage followUpMessage, List<String> images) {
+        return images.stream()
+                .map(url -> new FollowUpImage(followUpMessage, url))
+                .toList();
+    }
 }
--- question-service/src/main/java/com/exit/question/domain/question/FollowUpMessage.java
+++ question-service/src/main/java/com/exit/question/domain/question/FollowUpMessage.java
@@ -3,6 +3,7 @@ package com.exit.question.domain.question;
 import com.exit.common.domain.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.BatchSize;
@@ -15,11 +16,12 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @AttributeOverride(name = "createdAt", column = @Column(name = "follow_up_message_created_at"))
-@AttributeOverride(name = "updatedAt", column = @Column(name = "follow_up_message__updated_at"))
+@AttributeOverride(name = "updatedAt", column = @Column(name = "follow_up_message_updated_at"))
 public class FollowUpMessage extends BaseEntity {
 
     @Id
     @Column(name = "follow_up_message_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long followUpMessageId;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
@@ -35,4 +37,18 @@ public class FollowUpMessage extends BaseEntity {
     @OneToMany(mappedBy = "followUpMessage", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @BatchSize(size = 50)
     private List<FollowUpImage> followUpImages = new ArrayList<>();
+
+    @Builder
+    public FollowUpMessage(FollowUpRoom followUpRoom, Long followUpMessageWriterId, String followUpMessageContent, List<FollowUpImage> followUpImages) {
+        this.followUpRoom = followUpRoom;
+        this.followUpMessageWriterId = followUpMessageWriterId;
+        this.followUpMessageContent = followUpMessageContent;
+        this.followUpImages = followUpImages;
+    }
+
+    public List<String> getImageUrlList() {
+        return followUpImages.stream()
+                .map(FollowUpImage::getFollowUpImageUrl)
+                .toList();
+    }
 }
