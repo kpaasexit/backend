6ab9798cccf20653a048356783a6ec8130a2befd
taeyeongkims <xodud8530@gmail.com>
2025-09-27 15:58:33 +0900

    feat: 추가질문 컨트롤러 dto 생성

---
+++ gateway-service/src/main/java/com/exit/gateway/controller/question/dto/request/question/CreateAdditionalQuestionMessageRequestDto.java
@@ -0,0 +1,27 @@
+package com.exit.gateway.controller.question.dto.request.question;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateAdditionalQuestionMessageRequestDto {
+
+    @NotNull(message = "질문 ID는 필수입니다.")
+    private Long questionId;
+
+    @NotNull(message = "응답 ID는 필수입니다.")
+    private Long responseId;
+
+    @NotBlank(message = "메시지 내용은 필수입니다.")
+    private String content;
+
+    private List<MultipartFile> images;
+}
