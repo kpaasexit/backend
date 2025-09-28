90f76f38a01984d7c3e2cf59e43051d365ffa071
taeyeongkims <xodud8530@gmail.com>
2025-09-28 16:27:35 +0900

    feat: 댓글 proto 파일 수정

---
+++ question-service/src/main/java/com/exit/question/domain/Comment.java
@@ -0,0 +1,10 @@
+package com.exit.question.domain;
+
+import java.time.LocalDateTime;
+
+public interface Comment {
+    Long getCommentId();
+    Long getAuthorId();
+    String getContent();
+    LocalDateTime getCreatedAt();
+}
