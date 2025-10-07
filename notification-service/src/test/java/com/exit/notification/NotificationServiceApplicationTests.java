40353571c903631272bc158ee3ebc8e0847febbb
taeyeongkims <xodud8530@gmail.com>
2025-10-07 14:45:39 +0900

    test : 게이트웨이 snippets 파일 커밋

--- notification-service/src/test/java/com/exit/notification/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/exit/notification/NotificationServiceApplicationTests.java
@@ -1,9 +1,23 @@
 package com.exit.notification;
 
+import com.exit.notification.config.FirebaseConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
+@ComponentScan(excludeFilters = @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = FirebaseConfig.class
+))
 class NotificationServiceApplicationTests {
 
     @Test
