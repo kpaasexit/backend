9003165611bdf075f93961e5c684b579d7392303
tae0u0 <xodud8530@gmail.com>
2025-10-16 17:32:04 +0900

    feat : 토큰 만료 검증할 때, 유효하지 않은 토큰 에러 처리 가능하도록 수정

--- common/src/main/java/com/exit/common/auth/jwt/JwtTokenProvider.java
+++ common/src/main/java/com/exit/common/auth/jwt/JwtTokenProvider.java
@@ -81,7 +81,7 @@ public class JwtTokenProvider {
             Claims claims = getClaimsByToken(token);
             return claims.getExpiration().before(new Date());
         } catch (ExpiredJwtException e) {
-            return false;
+            throw new RestApiException(UserErrorCode.INVALID_TOKEN);
         }
     }
 
