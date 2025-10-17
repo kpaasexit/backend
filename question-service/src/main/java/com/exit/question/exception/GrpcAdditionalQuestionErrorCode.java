320e20c55edc11483ad11333157db7b2f5f8dd21
taeyeongkims <xodud8530@gmail.com>
2025-10-17 20:27:04 +0900

    feat : 최신 스닙셋

---
+++ question-service/src/main/java/com/exit/question/exception/GrpcAdditionalQuestionErrorCode.java
@@ -0,0 +1,23 @@
+package com.exit.question.exception;
+
+import com.exit.common.response.error.grpc.GrpcErrorCode;
+import io.grpc.Status;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum GrpcAdditionalQuestionErrorCode implements GrpcErrorCode {
+    // 추가 질문 관련
+    NULL_FOLLOW_UP_ROOM(Status.Code.NOT_FOUND, "ADDITIONAL_QUESTION_ERR_001", "추가 질문방이 존재하지 않음"),
+    NULL_FOLLOW_UP_MESSAGE(Status.Code.NOT_FOUND, "ADDITIONAL_QUESTION_ERR_002", "추가 질문 메시지가 존재하지 않음"),
+
+    // 추가 질문 작업 실패
+    CREATE_ADDITIONAL_QUESTION_MESSAGE_FAILED(Status.Code.INTERNAL, "ADDITIONAL_QUESTION_ERR_010", "추가 질문 메시지 생성에 실패했습니다."),
+    GET_ADDITIONAL_QUESTION_FAILED(Status.Code.INTERNAL, "ADDITIONAL_QUESTION_ERR_011", "추가 질문 조회에 실패했습니다."),
+    ;
+
+    private final Status.Code grpcStatusCode;
+    private final String developCode;
+    private final String errorDescription;
+}
