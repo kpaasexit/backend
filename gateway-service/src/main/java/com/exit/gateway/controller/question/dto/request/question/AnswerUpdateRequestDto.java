e551307a67243791fe33efdc26e929781dce2c48
taeyeongkims <xodud8530@gmail.com>
2025-10-26 02:28:49 +0900

    feat : 답변 수정 dto record에서 class로 변경

--- gateway-service/src/main/java/com/exit/gateway/controller/question/dto/request/question/AnswerUpdateRequestDto.java
+++ gateway-service/src/main/java/com/exit/gateway/controller/question/dto/request/question/AnswerUpdateRequestDto.java
@@ -1,11 +1,16 @@
 package com.exit.gateway.controller.question.dto.request.question;
 
-import jakarta.validation.constraints.NotBlank;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public record AnswerUpdateRequestDto(
-        @NotBlank(message = "답변 내용은 필수입니다.")
-        String content,
-        List<Long> deleteIds
-) {
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AnswerUpdateRequestDto {
+    private String content;
+    private List<Long> deleteIds;
 }
